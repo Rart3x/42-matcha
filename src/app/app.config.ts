05ff9fcefeceb75dee568e01f45155459f885e5c
tdubois <tdubois@student.42angouleme.fr>
2024-07-23 21:40:14 +0200

    feat: configure application

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+    provideClientHydration,
+    withEventReplay,
+    withI18nSupport,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
-        provideClientHydration(),
+        provideRouter(routes, withComponentInputBinding()),
+        provideClientHydration(withEventReplay(), withI18nSupport()),
+        provideHttpClient(withFetch()),
     ],
 };

0569279e062d88df12d52c7f9ed610a55928bd14
thibaut dubois <thibautdbs@outlook.com>
2024-07-25 14:10:36 +0200

    feat: home page and catch all route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,29 @@ import { Routes } from '@angular/router';
 
 import { routes as auth } from '@app/core/auth/auth.routes';
 import { isLoggedOutGuard } from '@app/core/auth/guards/is-logged-out.guard';
+import { isLoggedInGuard } from '@app/core/auth/guards/is-logged-in.guard';
 
 export const routes: Routes = [
     {
         path: '',
         children: auth,
         canActivateChild: [isLoggedOutGuard],
+    },
+    {
+        path: '',
+        children: [
+            {
+                path: 'home',
+                loadComponent: () =>
+                    import('@app/features/home-page/home-page.component').then(
+                        (m) => m.HomePageComponent,
+                    ),
+            },
+        ],
+        canActivateChild: [isLoggedInGuard],
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
     },
 ];

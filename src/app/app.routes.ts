8f65ae2bdbcb1a70812905e627a305c7327bbe8a
thibaut dubois <thibautdbs@outlook.com>
2024-07-25 11:50:49 +0200

    feat: login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { routes as auth } from '@app/core/auth/auth.routes';
+import { isLoggedOutGuard } from '@app/core/auth/guards/is-logged-out.guard';
 
 export const routes: Routes = [
     {
         path: '',
         children: auth,
+        canActivateChild: [isLoggedOutGuard],
     },
 ];

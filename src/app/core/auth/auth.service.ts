32f16fc80057524a6d09c25010db41ade85d8da5
thibaut dubois <thibautdbs@outlook.com>
2024-07-25 10:54:09 +0200

    feat: add template auth service

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,69 @@
+import { inject, Injectable } from '@angular/core';
+import {
+    catchError,
+    map,
+    merge,
+    Observable,
+    of,
+    ReplaySubject,
+    shareReplay,
+} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LoggerService } from '@app/core/services/logger.service';
+
+/**
+ * Service that handles the authentication of the user.
+ */
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    #http = inject(HttpClient);
+    #logger = inject(LoggerService);
+
+    #isAuthenticatedSubject = new ReplaySubject<boolean>(1);
+
+    /**
+     * Observable that emits the authentication status.
+     */
+    isAuthenticated$: Observable<boolean> = merge(
+        this.#ping(),
+        this.#isAuthenticatedSubject,
+    ).pipe(shareReplay(1));
+
+    /**
+     * Logs in the user.
+     *
+     * @param username
+     * @param password
+     * @returns An observable that emits `true` if the login was successful, `false` otherwise.
+     */
+    login(username: string, password: string): Observable<boolean> {
+        this.#logger.info(`Logging in as ${username}...`);
+        // TODO: Implement the login logic
+        return of(true);
+    }
+
+    /**
+     * Logs out the user.
+     *
+     * @returns An observable that emits `true` if the logout was successful, `false` otherwise.
+     */
+    logout(): Observable<boolean> {
+        this.#logger.info('Logging out...');
+        // TODO: Implement the logout logic
+        return of(true);
+    }
+
+    /**
+     * Pings the server to check if the user is authenticated.
+     *
+     * @private
+     */
+    #ping(): Observable<boolean> {
+        return this.#http.get('/api/ping').pipe(
+            map(() => true),
+            catchError(() => of(false)),
+        );
+    }
+}

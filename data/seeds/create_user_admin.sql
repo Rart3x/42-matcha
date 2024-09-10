d112106a4f15a40d60879d606f6178ded6c95ee2
tdubois <tdubois@student.42angouleme.fr>
2024-09-10 22:33:09 +0200

    fix create user admin seed script

--- data/seeds/create_user_admin.sql
+++ data/seeds/create_user_admin.sql
@@ -1,6 +1,6 @@
 DELETE
 FROM users
-WHERE email = 'admin@localhost';
+WHERE email = 'admin@localhost.com';
 
 INSERT INTO users (email, password, username, first_name, last_name)
 VALUES ('admin@localhost.com', crypt('Password1234@', gen_salt('bf',8)), 'admin', 'Admin', 'User');

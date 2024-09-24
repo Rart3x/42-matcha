46ecc8bbd37f9eb7eb61382bb2239f8c8454bb1c
tdubois <tdubois@student.42angouleme.fr>
2024-09-24 22:35:35 +0200

    fix: strengthen location validation

--- src/api/validators/location.validators.ts
+++ src/api/validators/location.validators.ts
@@ -1,14 +1,20 @@
 import { badRequest } from '@api/errors/bad-request.error';
 
 export async function validateLatitude(latitude?: number) {
-    if (latitude == null || latitude < -90 || latitude > 90) {
+    if (typeof latitude !== 'number') {
+        throw badRequest();
+    }
+    if (latitude < -90 || latitude > 90) {
         throw badRequest();
     }
     return latitude;
 }
 
 export async function validateLongitude(longitude?: number) {
-    if (longitude == null || longitude < -180 || longitude > 180) {
+    if (typeof longitude !== 'number') {
+        throw badRequest();
+    }
+    if (longitude < -180 || longitude > 180) {
         throw badRequest();
     }
     return longitude;

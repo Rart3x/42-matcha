10e3ab5ef787981fd445add93a112f83a7474229
Rart3x <ramjattankeny971@gmail.com>
2024-10-02 22:18:54 +0200

    feat: seed for locations to random users

--- src/api/dev-tools/seed.ts
+++ src/api/dev-tools/seed.ts
@@ -200,6 +200,20 @@ async function seed() {
         LIMIT 500;
     `;
 
+    console.log('seeding users/locations...');
+
+    await sql`
+        WITH random_users AS (
+            SELECT id 
+            FROM users
+            ORDER BY RANDOM()
+            LIMIT 500
+        )
+        INSERT INTO locations (user_id, latitude, longitude)
+        SELECT id, RANDOM() * 180 - 90, RANDOM() * 360 - 180
+        FROM random_users;
+    `;
+
     process.exit(0);
 }
 

0b0f3e68327068bfe3406a92ebb79667071934d9
tdubois <tdubois@student.42angouleme.fr>
2024-10-07 20:14:34 +0200

    fix: message seed

--- src/api/dev-tools/seed_messages.ts
+++ src/api/dev-tools/seed_messages.ts
@@ -2,27 +2,59 @@ import { sql } from '@api/connections/database.connection';
 import { faker } from '@faker-js/faker/locale/en';
 
 export async function seedMessages() {
-    const messages = Array.from({ length: 500 }, () => faker.lorem.sentence());
-
-    await sql`
-        WITH random_users AS (
-            SELECT id 
-            FROM users
-            ORDER BY RANDOM()
-            LIMIT 500
-        ),
-        messages_candidates AS (
-            SELECT u1.id AS sender_id, u2.id AS receiver_id,
-                   ROW_NUMBER() OVER (PARTITION BY u1.id ORDER BY RANDOM()) AS rn
-            FROM random_users u1
-            JOIN random_users u2 ON u1.id <> u2.id
-        )
-        INSERT INTO messages (sender_id, receiver_id, message)
-        SELECT sender_id, receiver_id, unnest(${sql.array(messages)})
-        FROM messages_candidates
-        WHERE rn <= 10
+    const pairs = await sql<
+        {
+            user_1: number;
+            user_2: number;
+        }[]
+    >`
+        WITH
+            random_users_1 AS (
+                SELECT
+                    id
+                FROM users
+                ORDER BY
+                    RANDOM()
+            ),
+            random_users_2 AS (
+                SELECT
+                    id
+                FROM users
+                ORDER BY
+                    RANDOM()
+            )
+        SELECT
+            u1.id AS user_1,
+            u2.id AS user_2
+        FROM random_users_1 u1
+            JOIN random_users_2 u2
+                ON u1.id <> u2.id
         LIMIT 500;
     `;
+
+    for (const { user_1, user_2 } of pairs) {
+        const numMessages = faker.number.int({
+            min: 1,
+            max: 50,
+        });
+
+        const messages = Array.from({ length: numMessages }, () => {
+            const sender_id = faker.datatype.boolean() ? user_1 : user_2;
+            const receiver_id = sender_id === user_1 ? user_2 : user_1;
+            const message = faker.lorem.sentence();
+
+            return {
+                sender_id,
+                receiver_id,
+                message,
+            };
+        });
+
+        await sql`
+            INSERT INTO
+                messages ${sql(messages)}
+        `;
+    }
 }
 
 async function seed() {

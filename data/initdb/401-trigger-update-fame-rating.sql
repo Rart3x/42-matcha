86c20b0eb5097005f39f0ddee9658c82d9e8f8de
tdubois <tdubois@student.42angouleme.fr>
2024-10-07 20:24:05 +0200

    feat: update fame_rating weights

--- data/initdb/401-trigger-update-fame-rating.sql
+++ data/initdb/401-trigger-update-fame-rating.sql
@@ -25,58 +25,66 @@ $$ LANGUAGE plpgsql;
 
 ------------------------------------------------------------------------------------------------------------------------
 
+-- Likes
+
 --- Description: Upgrades the fame rating of a user when a like is inserted.
 CREATE OR REPLACE TRIGGER upgrade_fame_rating_trigger
     AFTER INSERT
     ON likes
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('liked_user_id', '1');
+EXECUTE FUNCTION update_fame_rating_trigger('liked_user_id', '25');
 
 --- Description: Downgrades the fame rating of a user when a like is deleted.
 CREATE OR REPLACE TRIGGER downgrade_fame_rating_trigger
     AFTER DELETE
     ON likes
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('liked_user_id', '-1');
+EXECUTE FUNCTION update_fame_rating_trigger('liked_user_id', '-25');
+
+-- Visits
 
 --- Description: Upgrades the fame rating of a user when a visit is inserted.
 CREATE OR REPLACE TRIGGER upgrade_fame_rating_trigger
     AFTER INSERT
     ON visits
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('visited_user_id', '1');
+EXECUTE FUNCTION update_fame_rating_trigger('visited_user_id', '5');
 
 -- Description: Downgrades the fame rating of a user when a visit is deleted.
 CREATE OR REPLACE TRIGGER downgrade_fame_rating_trigger
     AFTER DELETE
     ON visits
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('visited_user_id', '-1');
+EXECUTE FUNCTION update_fame_rating_trigger('visited_user_id', '-5');
+
+-- Reports
 
 --- Description: Downgrades the fame rating of a user when a fake user report is inserted.
 CREATE OR REPLACE TRIGGER downgrade_fame_rating_trigger
     AFTER INSERT
     ON fake_user_reports
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('reported_user_id', '-1');
+EXECUTE FUNCTION update_fame_rating_trigger('reported_user_id', '-100');
 
 --- Description: Downgrades the fame rating of a user when a fake user report is deleted.
 CREATE OR REPLACE TRIGGER downgrade_fame_rating_trigger
     AFTER DELETE
     ON fake_user_reports
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('reported_user_id', '1');
+EXECUTE FUNCTION update_fame_rating_trigger('reported_user_id', '100');
+
+-- Blocks
 
 --- Description: Downgrades the fame rating of a user when a block is inserted.
 CREATE OR REPLACE TRIGGER downgrade_fame_rating_trigger
     AFTER INSERT
     ON blocks
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('blocked_user_id', '-1');
+EXECUTE FUNCTION update_fame_rating_trigger('blocked_user_id', '-25');
 
 --- Description: Upgrades the fame rating of a user when a block is deleted.
 CREATE OR REPLACE TRIGGER upgrade_fame_rating_trigger
     AFTER DELETE
     ON blocks
     FOR EACH ROW
-EXECUTE FUNCTION update_fame_rating_trigger('blocked_user_id', '1');
+EXECUTE FUNCTION update_fame_rating_trigger('blocked_user_id', '25');

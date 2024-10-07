581afd94aa65d3e53d32b2863ac2e4a4ac5201ae
tdubois <tdubois@student.42angouleme.fr>
2024-10-07 20:33:19 +0200

    fix: location seed

--- src/api/dev-tools/seed_locations.ts
+++ src/api/dev-tools/seed_locations.ts
@@ -1,16 +1,35 @@
 import { sql } from '@api/connections/database.connection';
+import { faker } from '@faker-js/faker/locale/en';
 
 export async function seedLocations() {
+    const users = await sql<
+        {
+            id: number;
+        }[]
+    >`
+        SELECT id
+        FROM users
+        ORDER BY RANDOM()
+        LIMIT 1800;
+    `;
+
+    const locations = users.map((user) => {
+        const coords = faker.location.nearbyGPSCoordinate({
+            origin: [45.83879541447026, 1.2633053879004603],
+            isMetric: true, // use km
+            radius: 200,
+        });
+
+        return {
+            user_id: user.id,
+            latitude: coords[0],
+            longitude: coords[1],
+        };
+    });
+
     await sql`
-        WITH random_users AS (
-            SELECT id 
-            FROM users
-            ORDER BY RANDOM()
-            LIMIT 500
-        )
-        INSERT INTO locations (user_id, latitude, longitude)
-        SELECT id, RANDOM() * 180 - 90, RANDOM() * 360 - 180
-        FROM random_users;
+        INSERT INTO locations ${sql(locations)}
+        ON CONFLICT DO NOTHING; 
     `;
 }
 

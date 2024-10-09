6665bdb67980b52a9b174d2a0c9b6aebd16c626d
Rart3x <ramjattankeny971@gmail.com>
2024-10-09 19:50:58 +0200

    feat: 3 pictures for each user

--- src/api/dev-tools/seed_pictures.ts
+++ src/api/dev-tools/seed_pictures.ts
@@ -1,6 +1,42 @@
 import { sql } from '@api/connections/database.connection';
 
-export async function seedPictures() {
+export async function seedPicturesSlot0() {
+    await sql`
+    WITH random_users AS (
+        SELECT id
+        FROM users
+        ORDER BY random()
+        LIMIT 2000
+    )
+    INSERT INTO pictures (user_id, position, url, mime_type)
+    SELECT
+        id,
+        2,
+        'public/profile_pictures/avatar' || id % 37 || '.jpg',
+        'image/jpeg'
+    FROM random_users
+    `;
+}
+
+export async function seedPicturesSlot1() {
+    await sql`
+    WITH random_users AS (
+        SELECT id
+        FROM users
+        ORDER BY random()
+        LIMIT 2000
+    )
+    INSERT INTO pictures (user_id, position, url, mime_type)
+    SELECT
+        id,
+        3,
+        'public/profile_pictures/avatar' || id % 3 || '.jpg',
+        'image/jpeg'
+    FROM random_users
+    `;
+}
+
+export async function seedPicturesSlot2() {
     await sql`
     WITH random_users AS (
         SELECT id
@@ -19,7 +55,9 @@ export async function seedPictures() {
 }
 
 async function seed() {
-    await seedPictures();
+    await seedPicturesSlot0();
+    await seedPicturesSlot1();
+    await seedPicturesSlot2();
 
     process.exit(0);
 }

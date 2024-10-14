cd2ee1f1d616b740251eb7ad7c5c1d2b5da7c617
thibaut dubois <thibautdbs@outlook.com>
2024-10-14 15:31:27 +0200

    fix: make routeAnimation optional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,19 +7,23 @@ import { MatIcon } from '@angular/material/icon';
 
 const blinkAnimation = () => [
     style({ position: 'relative' }),
-    query(':enter, :leave', [
-        style({
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-        }),
-    ]),
-    query(':enter', [style({ opacity: 0 })]),
-    query(':leave', [style({ opacity: 1 })]),
+    query(
+        ':enter, :leave',
+        [
+            style({
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+            }),
+        ],
+        { optional: true },
+    ),
+    query(':enter', [style({ opacity: 0 })], { optional: true }),
+    query(':leave', [style({ opacity: 1 })], { optional: true }),
     group([
-        query(':leave', [animate('0.1s', style({ opacity: 0 }))]),
-        query(':enter', [animate('0.1s', style({ opacity: 1 }))]),
+        query(':leave', [animate('0.1s', style({ opacity: 0 }))], { optional: true }),
+        query(':enter', [animate('0.1s', style({ opacity: 1 }))], { optional: true }),
     ]),
 ];
 

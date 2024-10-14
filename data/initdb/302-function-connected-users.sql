6db79eb589979c007626b7afd461cc06d8579fd4
thibaut dubois <thibautdbs@outlook.com>
2024-10-14 19:39:38 +0200

    fix: only reachable users should be connected

--- data/initdb/302-function-connected-users.sql
+++ data/initdb/302-function-connected-users.sql
@@ -2,15 +2,13 @@
 --- n.b. two users are connected if they share mutual likes.
 CREATE OR REPLACE FUNCTION connected_users(principal_user_id INTEGER) RETURNS SETOF users AS
 $$
-SELECT
-    other_user.*
-FROM users AS other_user
-    INNER JOIN likes AS like_from_principal_user
-        ON like_from_principal_user.liker_user_id = principal_user_id AND
-           like_from_principal_user.liked_user_id = other_user.id
-    INNER JOIN likes AS like_to_principal_user
-        ON like_to_principal_user.liked_user_id = principal_user_id AND
-           like_to_principal_user.liker_user_id = other_user.id
-WHERE
-    other_user.id != principal_user_id
-$$ LANGUAGE SQL;
+SELECT other_user.*
+  FROM reachable_users(principal_user_id) AS other_user
+           INNER JOIN likes AS like_from_principal_user
+           ON like_from_principal_user.liker_user_id = principal_user_id AND
+              like_from_principal_user.liked_user_id = other_user.id
+           INNER JOIN likes AS like_to_principal_user
+           ON like_to_principal_user.liked_user_id = principal_user_id AND
+              like_to_principal_user.liker_user_id = other_user.id
+ WHERE other_user.id != principal_user_id
+$$ LANGUAGE sql;

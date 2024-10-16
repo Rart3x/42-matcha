a83aaf211609e6c5c43d95c71a8684cb068199b5
Rart3x <ramjattankeny971@gmail.com>
2024-10-16 21:23:40 +0200

    feat: new pass validator

---
+++ src/api/validators/password_modifications.validators.ts
@@ -0,0 +1,12 @@
+import { badRequest } from '@api/errors/bad-request.error';
+
+export async function validateNewPassword(newPass?: string, password?: string) {
+    if (typeof password !== 'string') {
+        throw badRequest();
+    }
+
+    if (newPass === password) {
+        throw badRequest();
+    }
+    return password;
+}

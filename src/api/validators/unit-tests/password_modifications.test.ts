2452b9085dd13559c65189610c9fb7ed67bc0aa6
Rart3x <ramjattankeny971@gmail.com>
2024-10-16 21:27:44 +0200

    feat: unit tests for password modifications

---
+++ src/api/validators/unit-tests/password_modifications.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from 'vitest';
+import { validateNewPassword } from '@api/validators/password_modifications.validators';
+import { validateOffset } from '@api/validators/page.validators';
+
+// -- NEW PASSWORD TESTS -- //
+test('[INVALID] [NEWPASSWORD]: pass -> pass', async () => {
+    await expect(validateNewPassword('pass', 'pass')).rejects.toThrow();
+});
+
+test('[VALID] [NEWPASSWORD]: pass -> pass2', async () => {
+    expect(await validateNewPassword('pass2', 'pass')).toEqual('pass');
+});
